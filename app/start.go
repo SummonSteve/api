19d250c4c7bff45f22964be46a5ad1bf40d33800
Nouko61 nao_ke@qq.com <nao_ke@qq.com>
2020-02-16 18:40:35 +0800

    rxbp

--- app/start.go
+++ app/start.go
@@ -97,6 +97,7 @@ func Start(conf common.Conf, dbO *sqlx.DB) *fhr.Router {
 		r.Method("/api/v1/users/userpage", v1.UserUserpageGET)
 		r.Method("/api/v1/users/lookup", v1.UserLookupGET)
 		r.Method("/api/v1/users/scores/best", v1.UserScoresBestGET)
+		r.Method("/api/v1/users/scores/rxbest", v1.UserRXScoresBestGET)
 		r.Method("/api/v1/users/scores/recent", v1.UserScoresRecentGET)
 		r.Method("/api/v1/badges", v1.BadgesGET)
 		r.Method("/api/v1/badges/members", v1.BadgeMembersGET)
--- app/v1/user_scores.go
+++ app/v1/user_scores.go
@@ -38,6 +38,25 @@ INNER JOIN beatmaps ON beatmaps.beatmap_md5 = scores.beatmap_md5
 INNER JOIN users ON users.id = scores.userid
 `
 
+const userRXScoreSelectBase = `
+SELECT
+	scores_relax.id, scores_relax.beatmap_md5, scores_relax.score,
+	scores_relax.max_combo, scores_relax.full_combo, scores_relax.mods,
+	scores_relax.300_count, scores_relax.100_count, scores_relax.50_count,
+	scores_relax.gekis_count, scores_relax.katus_count, scores_relax.misses_count,
+	scores_relax.time, scores_relax.play_mode, scores_relax.accuracy, scores_relax.pp,
+	scores_relax.completed,
+
+	beatmaps.beatmap_id, beatmaps.beatmapset_id, beatmaps.beatmap_md5,
+	beatmaps.song_name, beatmaps.ar, beatmaps.od, beatmaps.difficulty_std,
+	beatmaps.difficulty_taiko, beatmaps.difficulty_ctb, beatmaps.difficulty_mania,
+	beatmaps.max_combo, beatmaps.hit_length, beatmaps.ranked,
+	beatmaps.ranked_status_freezed, beatmaps.latest_update
+FROM scores_relax
+INNER JOIN beatmaps ON beatmaps.beatmap_md5 = scores.beatmap_md5
+INNER JOIN users ON users.id = scores.userid
+`
+
 // UserScoresBestGET retrieves the best scores of an user, sorted by PP if
 // mode is standard and sorted by ranked score otherwise.
 func UserScoresBestGET(md common.MethodData) common.CodeMessager {
@@ -57,6 +76,29 @@ func UserScoresBestGET(md common.MethodData) common.CodeMessager {
 			%s
 			AND `+md.User.OnlyUserPublic(true)+`
 		ORDER BY scores.pp DESC, scores.score DESC %s`,
+		wc, mc, common.Paginate(md.Query("p"), md.Query("l"), 100),
+	), param)
+}
+
+// UserRXScoresBestGET retrieves the best scores of an user, sorted by PP if
+// mode is standard and sorted by ranked score otherwise.
+func UserRXScoresBestGET(md common.MethodData) common.CodeMessager {
+	cm, wc, param := whereClauseUser(md, "users")
+	if cm != nil {
+		return *cm
+	}
+	mc := genModeClause(md)
+	// For all modes that have PP, we leave out 0 PP scores.
+	if getMode(md.Query("mode")) != "ctb" {
+		mc += " AND scores_relax.pp > 0"
+	}
+	return scoresPuts(md, fmt.Sprintf(
+		`WHERE
+			scores_relax.completed = '3'
+			AND %s
+			%s
+			AND `+md.User.OnlyUserPublic(true)+`
+		ORDER BY scores_relax.pp DESC, scores_relax.score DESC %s`,
 		wc, mc, common.Paginate(md.Query("p"), md.Query("l"), 100),
 	), param)
 }
